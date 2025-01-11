2d1a650f3d51498040725351365aec33c649c9cb
Cinnazeyy <imcinnazeyy@gmail.com>
2025-01-11 02:00:15 +0100

    add PlotSystem-API project

---
+++ PlotSystem-API/Models/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace PlotSystem_API.Models;
+
+public enum Status
+{
+    UNCLAIMED,
+    UNFINISHED,
+    UNREVIEWED,
+    COMPLETED
+}
---
+++ PlotSystem-API/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
